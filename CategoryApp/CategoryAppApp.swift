2d0d57bd975338fb2b3db56545d004c7e4f95fdd
Mohamed Ali <Mohamed.ali@yassir.com>
2023-12-02 00:27:38 +0200

    Initial Commit

---
+++ CategoryApp/CategoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryAppApp.swift
+//  CategoryApp
+//
+//  Created by Mohamed Korany Ali on 02/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CategoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
