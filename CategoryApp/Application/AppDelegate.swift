04b810fef54fde6e2ccac51b1476f8c0403db39a
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:38:52 +0200

    Update AppDelegate.swift

--- CategoryApp/Application/AppDelegate.swift
+++ CategoryApp/Application/AppDelegate.swift
@@ -5,4 +5,27 @@
 //  Created by Mohamed Korany Ali on 02/12/2023.
 //
 
-import Foundation
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    private var coordinator: CategoryListCoordinatorProtocol?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupMainWindow()
+        return true
+    }
+    
+    private func setupMainWindow() {
+        let window = UIWindow()
+        window.makeKeyAndVisible()
+        self.window = window
+        let navigationConroller = UINavigationController()
+        self.window?.rootViewController = navigationConroller
+        coordinator = CategoryListCoordinator(navigationController: navigationConroller)
+        coordinator?.start()
+      }
+}
