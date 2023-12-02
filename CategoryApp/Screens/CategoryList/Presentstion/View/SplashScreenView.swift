575b3c48c6e21d0dac832da30464fa03a13447fd
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:34:35 +0200

    Add SplashScreenView

--- CategoryApp/Screens/CategoryList/Presentstion/View/SplashScreenView.swift
+++ CategoryApp/Screens/CategoryList/Presentstion/View/SplashScreenView.swift
@@ -5,4 +5,43 @@
 //  Created by Mohamed Korany Ali on 02/12/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State var isActive : Bool = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    var coordinator: CategoryListCoordinatorProtocol
+    
+    var body: some View {
+        if isActive {
+            CategoryListView(coordinator: coordinator)
+        } else {
+            VStack {
+                VStack {
+                    Image("swenson-he-logo")
+                        .font(.system(size: 80))
+                        
+                    Text("Swenson He Test App")
+                        .font(Font.custom("Baskerville-Bold", size: 26))
+                        .foregroundColor(.black.opacity(0.80))
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.2)) {
+                        self.size = 0.9
+                        self.opacity = 1.00
+                    }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
