98ce5d48cab5dc51e02343779d6c3fe1a2a4fdfa
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:37:47 +0200

    Add LoadCategoryDetailsUseCase

--- CategoryApp/Screens/CategoryDetailsList/Domain/UseCase/LoadCategoryDetailsUseCase.swift
+++ CategoryApp/Screens/CategoryDetailsList/Domain/UseCase/LoadCategoryDetailsUseCase.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+protocol LoadCategoryDetailsUseCaseContract {
+    func execute(id: String) async -> Result<CategroyDetailsListResponse, Error>
+}
+
+class LoadCategoryDetailsUseCase: LoadCategoryDetailsUseCaseContract {
+
+    private let repository: CategoryDetailsListRepositoryContract
+    
+    init(repository: CategoryDetailsListRepositoryContract = CategoryDetailsListRepository()) {
+        self.repository = repository
+    }
+    
+    func execute(id: String) async -> Result<CategroyDetailsListResponse, Error> {
+       await repository.loadCategoryDetails(id: id)
+    }
+}
