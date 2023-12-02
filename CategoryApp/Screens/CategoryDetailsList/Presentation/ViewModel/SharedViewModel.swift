038e81bc70db918165478f7f8f85b616200584c9
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:37:22 +0200

    Add SharedViewModel

--- CategoryApp/Screens/CategoryDetailsList/Presentation/ViewModel/SharedViewModel.swift
+++ CategoryApp/Screens/CategoryDetailsList/Presentation/ViewModel/SharedViewModel.swift
@@ -5,4 +5,17 @@
 //  Created by Mohamed Korany Ali on 02/12/2023.
 //
 
-import Foundation
+import Combine
+
+class SharedViewModel: BaseViewModel {
+    @Published var selectedItems: CategroyDetailsListResponse = []
+    var priceAsString: String {
+        return price > .zero ? "$" + String(price) : "-"
+    }
+    
+    var price: Int {
+        selectedItems.compactMap { $0.avgBudget }.reduce(0, +)
+    }
+    
+    @Published var selectedCategories: [Int: Int] = [:]
+}
