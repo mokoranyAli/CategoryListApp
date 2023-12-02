e024597159eb3b2750cd25405cab24f07bca1713
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:35:25 +0200

    Add CategoryListViewModel

--- CategoryApp/Screens/CategoryList/Presentstion/ViewModel/CategoryListViewModel.swift
+++ CategoryApp/Screens/CategoryList/Presentstion/ViewModel/CategoryListViewModel.swift
@@ -5,4 +5,42 @@
 //  Created by Mohamed Korany Ali on 02/12/2023.
 //
 
+import Combine
 import Foundation
+
+final class CategoryListViewModel: BaseViewModel {
+    
+    // MARK: - Properties
+    
+    @Published private(set) var categories: CategoryListResponse = []
+    private let loadCategoriesUseCase: LoadCategoriesUseCaseeContract
+    @Published var sharedViewmodel: SharedViewModel = .init()
+
+    // MARK: - Init
+    init(loadCategoriesUseCase: LoadCategoriesUseCaseeContract = LoadCategoriesUseCase()) {
+        self.loadCategoriesUseCase = loadCategoriesUseCase
+        super.init()
+        loadData()
+        sharedViewmodel.$selectedItems.receive(on: DispatchQueue.main).sink { _ in
+            self.objectWillChange.send()
+        }.store(in: &cancellables)
+    }
+    
+    // MARK: - Handlers
+    private func loadData() {
+        Task {
+            state = .loading
+            let result = await loadCategoriesUseCase.execute()
+            await MainActor.run {
+                switch result {
+                case .success(let list):
+                    categories = list
+                    state = .success
+                case .failure(let error):
+                    state = .failure(error)
+                    alertItem = state.alertItem
+                }
+            }
+        }
+    }
+}
