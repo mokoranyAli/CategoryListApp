65c8ee20133668da82e4481fc3ddd23e913220b0
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:43:09 +0200

    Add mocking for CategoryDetailsListService success and failure

--- CategoryAppTests/ScreenTests/CategoryDetailsList/Data/Repository/MockCategoryDetailsListServiceFailure.swift
+++ CategoryAppTests/ScreenTests/CategoryDetailsList/Data/Repository/MockCategoryDetailsListServiceFailure.swift
@@ -1,8 +1,15 @@
 //
-//  MockCategoryDetailsListRepositoryFailure.swift
+//  MockCategoryDetailsListServiceFailure.swift
 //  CategoryAppTests
 //
 //  Created by Mohamed Korany Ali on 02/12/2023.
 //
 
 import Foundation
+@testable import CategoryApp
+
+class MockCategoryDetailsListServiceFailure: CategoryDetailsListServiceContract {
+    func loadCategoryDetailsList(id: String) async -> Result<CategoryApp.CategroyDetailsListResponse, Error> {
+        .failure(NetworkError.invalidURL)
+    }
+}
--- CategoryAppTests/ScreenTests/CategoryDetailsList/Data/Repository/MockCategoryDetailsListServiceSuccess.swift
+++ CategoryAppTests/ScreenTests/CategoryDetailsList/Data/Repository/MockCategoryDetailsListServiceSuccess.swift
@@ -1,8 +1,15 @@
 //
-//  MockCategoryDetailsListRepositorySuccess.swift
+//  MockCategoryDetailsListServiceSuccess.swift
 //  CategoryAppTests
 //
 //  Created by Mohamed Korany Ali on 02/12/2023.
 //
 
 import Foundation
+@testable import CategoryApp
+
+class MockCategoryDetailsListServiceSuccess: CategoryDetailsListServiceContract {
+    func loadCategoryDetailsList(id: String) async -> Result<CategoryApp.CategroyDetailsListResponse, Error> {
+        .success(MockData.categoryDetailsListResponse)
+    }
+}
