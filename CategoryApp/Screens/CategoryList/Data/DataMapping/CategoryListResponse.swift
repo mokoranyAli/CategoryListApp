cff0d6062fc3636af1f9191639ceb400db7bdc20
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:32:27 +0200

    Add CategoryResponse

--- CategoryApp/Screens/CategoryList/Data/DataMapping/CategoryListResponse.swift
+++ CategoryApp/Screens/CategoryList/Data/DataMapping/CategoryListResponse.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+// MARK: - CategoryResponse
+struct CategoryResponse: Decodable, Hashable {
+    let id: Int?
+    let title: String?
+    let image: String?
+}
+
+typealias CategoryListResponse = [CategoryResponse]
