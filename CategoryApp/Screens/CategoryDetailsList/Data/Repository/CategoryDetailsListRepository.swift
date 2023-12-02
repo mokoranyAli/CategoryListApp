fd17c998934007f45c7e1ff9a03c5ea365addfb4
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:36:56 +0200

    Add CategoryDetailsListRepository

--- CategoryApp/Screens/CategoryDetailsList/Data/Repository/CategoryDetailsListRepository.swift
+++ CategoryApp/Screens/CategoryDetailsList/Data/Repository/CategoryDetailsListRepository.swift
@@ -5,4 +5,21 @@
 //  Created by Mohamed Korany Ali on 02/12/2023.
 //
 
-import Foundation
+import Combine
+
+protocol CategoryDetailsListRepositoryContract {
+    func loadCategoryDetails(id: String) async -> Result<CategroyDetailsListResponse, Error>
+}
+
+class CategoryDetailsListRepository: CategoryDetailsListRepositoryContract {
+
+    private let service: CategoryDetailsListServiceContract
+    
+    init(service: CategoryDetailsListServiceContract = CategoryDetailsListService()) {
+        self.service = service
+    }
+    
+    func loadCategoryDetails(id: String) async -> Result<CategroyDetailsListResponse, Error> {
+        await service.loadCategoryDetailsList(id: id) // Normally we are doing any cache operation and also we convert from Network Model to Domain Model here
+    }
+}
