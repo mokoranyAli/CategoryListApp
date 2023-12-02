90a1bf0c1f2815940de68226aa23684917b25970
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:33:00 +0200

    Add LoadCategoriesUseCase

--- CategoryApp/Screens/CategoryList/Domain/UseCase/LoadCategoriesUseCase.swift
+++ CategoryApp/Screens/CategoryList/Domain/UseCase/LoadCategoriesUseCase.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+protocol LoadCategoriesUseCaseeContract {
+    func execute() async -> Result<CategoryListResponse, Error>
+}
+
+class LoadCategoriesUseCase: LoadCategoriesUseCaseeContract {
+
+    private let repository: CategoryListRepositoryContract
+    
+    init(repository: CategoryListRepositoryContract = CategoryListRepository()) {
+        self.repository = repository
+    }
+    
+    func execute() async -> Result<CategoryListResponse, Error> {
+       await repository.loadCategories()
+    }
+}
