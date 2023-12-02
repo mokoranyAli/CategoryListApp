370126a9c1d929b1711c3ab9c89eb6fe84b582f2
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:40:21 +0200

    Update MockData.swift

--- CategoryAppTests/Mocks/MockData.swift
+++ CategoryAppTests/Mocks/MockData.swift
@@ -5,4 +5,18 @@
 //  Created by Mohamed Korany Ali on 02/12/2023.
 //
 
+
 import Foundation
+@testable import CategoryApp
+
+struct MockData {
+    
+    static let categoryListResponse: CategoryListResponse =  {
+        [.init(id: 123, title: "Item", image: "URL")]
+    }()
+    
+    
+    static let categoryDetailsListResponse: CategroyDetailsListResponse =  {
+        [.init(id: 123, title: "Item", minBudget: 5, maxBudget: 10, avgBudget: 7, image: "URL")]
+    }()
+}
