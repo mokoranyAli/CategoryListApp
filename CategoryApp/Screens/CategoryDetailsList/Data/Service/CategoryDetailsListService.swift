f802e5d07cff58be2795d82c30c13d1d9c1935c2
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:36:27 +0200

    Add CategoryDetailsListService

--- CategoryApp/Screens/CategoryDetailsList/Data/Service/CategoryDetailsListService.swift
+++ CategoryApp/Screens/CategoryDetailsList/Data/Service/CategoryDetailsListService.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+protocol CategoryDetailsListServiceContract {
+    func loadCategoryDetailsList(id: String) async -> Result<CategroyDetailsListResponse, Error>
+}
+
+class CategoryDetailsListService: CategoryDetailsListServiceContract {
+    
+    private let apiService: APIServiceContract
+    
+    init(apiService: APIServiceContract = APIService.shared) {
+        self.apiService = apiService
+    }
+    
+    func loadCategoryDetailsList(id: String) async -> Result<CategroyDetailsListResponse, Error> {
+         let baseURL = Environment.baseURL
+        guard  let url = URL(string: baseURL + Path.categories + id + ".json") else {
+           return .failure(NetworkError.invalidURL)
+       }
+        
+        return await apiService.request(url)
+    }
+}
+
+private extension CategoryDetailsListService {
+    enum Path {
+        static let categories = "/categories/"
+    }
+}
