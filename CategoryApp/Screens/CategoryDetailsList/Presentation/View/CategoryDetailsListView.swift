f6100e07171f0e4363d853f061555a6157b7de5f
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:38:19 +0200

    Add CategoryDetailsListView with its row

--- CategoryApp/Screens/CategoryDetailsList/Presentation/View/CategoryDetailsListView.swift
+++ CategoryApp/Screens/CategoryDetailsList/Presentation/View/CategoryDetailsListView.swift
@@ -7,14 +7,58 @@
 
 import SwiftUI
 
-struct CategoryDetailsListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+import SwiftUI
 
-struct CategoryDetailsListView_Previews: PreviewProvider {
-    static var previews: some View {
-        CategoryDetailsListView()
+struct CategoryDetailsListView: View {
+    @SwiftUI.Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @StateObject var viewModel: CategoryDetailsViewModel
+    var coordinator: CategoryListCoordinatorProtocol
+    let columns = [
+        GridItem(.flexible(), spacing: 18),
+        GridItem(.flexible()),
+    ]
+    init(coordinator: CategoryListCoordinatorProtocol, viewModel: CategoryDetailsViewModel) {
+        self.coordinator = coordinator
+        
+#if DEBUG
+        if UITestingHelper.isUITesting {
+            let uiTestingViewModel = CategoryDetailsViewModel(title: "", categoryID: .zero, sharedViewModel: viewModel.sharedViewmodel, loadCategoryDetailssUseCase: LoadCategoryDetailsUseCase(repository: CategoryDetailsListRepository(service: CategoryDetailsListService(apiService: MockAPIService(fileName: UITestingHelper.categoryDetailsListJson)))))
+            self._viewModel = StateObject(wrappedValue: uiTestingViewModel)
+        }
+        else {
+            self._viewModel =  StateObject(wrappedValue: viewModel)
+        }
+#else
+        self._viewModel = StateObject(wrappedValue: viewModel)
+#endif
+    }
+    var body: some View {
+        BaseView(state: $viewModel.state, alertItem: $viewModel.alertItem) {
+            ScrollView {
+                HeaderView(title: viewModel.title, price: .constant(viewModel.displayedPrice))
+                LazyVGrid(columns: columns, spacing: 18) {
+                    ForEach(viewModel.categoryItems, id: \.self) { item in
+                        DetailsItemRowView(item: item, selectedItems: $viewModel.sharedViewmodel.selectedItems)
+                            .onTapGesture {
+                                
+                            }
+                    }
+                }
+            }.padding(.vertical)
+                .padding(.horizontal, 16)
+                .navigationBarItems(leading: backButton)
+                .navigationBarBackButtonHidden(true)
+        }
+    }
+    
+    private var backButton: some View {
+        Button {
+            presentationMode.wrappedValue.dismiss()
+        } label: {
+            Image("BackBtn") // set image here
+                .resizable()
+                .scaledToFit()
+                .frame(width: 20, height: 20)
+        }
     }
 }
--- CategoryApp/Screens/CategoryDetailsList/Presentation/View/DetailsItemRowView.swift
+++ CategoryApp/Screens/CategoryDetailsList/Presentation/View/DetailsItemRowView.swift
@@ -8,13 +8,63 @@
 import SwiftUI
 
 struct DetailsItemRowView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    let item: CategroyDetailResponse
+    @Binding var selectedItems: [CategroyDetailResponse]
+    
+    private var isSelected: Bool {
+        selectedItems.contains(item)
     }
-}
-
-struct DetailsItemRowView_Previews: PreviewProvider {
-    static var previews: some View {
-        DetailsItemRowView()
+    
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            VStack(alignment: .leading) {
+                image
+                    .frame(maxWidth: .infinity, maxHeight: 104)
+                    .cornerRadius(4)
+                VStack(alignment: .leading) {
+                    Text(item.title ?? "")
+                        .font(Font.appFont(size: 14, weight: .regular))
+                    if let minBudget = item.minBudget, let maxBudget = item.maxBudget {
+                        Text("$\(minBudget) - \(maxBudget)")
+                            .font(Font.appFont(size: 14, weight: .black))
+                    }
+                }.padding()
+                    
+            }.background(Color.white)
+                .padding(2)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .shadow(radius: 1)
+            
+            Button {
+                selectedItems = isSelected ? selectedItems.filter { $0 != item} : selectedItems + [item]
+            } label: {
+                
+                additionIcon
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 35, height: 35)
+            }.padding()
+        }
+    }
+    
+    private var additionIcon: Image {
+        if isSelected {
+            return Image("itemIsAdded")
+        }
+        else {
+            return Image("AddItem")
+        }
+    }
+    
+    private var image: some View {
+        AsyncImage(url: .init(string: item.image ?? ""), content: { image in
+            image.resizable()
+                .scaledToFill()
+        }, placeholder: {
+            Image("placeholder-image-icon")
+                .resizable()
+                .scaledToFit()
+        })
     }
 }
