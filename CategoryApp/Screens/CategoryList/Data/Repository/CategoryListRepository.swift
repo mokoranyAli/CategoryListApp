f3b585b685fa8b9a2cf8ddecd3e9174c42aad692
Mohamed Ali <mohammed.korany.ali@gmail.com>
2023-12-02 20:32:12 +0200

    Add CategoryListService

--- CategoryApp/Screens/CategoryList/Data/Repository/CategoryListRepository.swift
+++ CategoryApp/Screens/CategoryList/Data/Repository/CategoryListRepository.swift
@@ -5,4 +5,21 @@
 //  Created by Mohamed Korany Ali on 02/12/2023.
 //
 
-import Foundation
+import Combine
+
+protocol CategoryListRepositoryContract {
+    func loadCategories() async -> Result<CategoryListResponse, Error>
+}
+
+class CategoryListRepository: CategoryListRepositoryContract {
+
+    private let service: CategoryListServiceContract
+    
+    init(service: CategoryListServiceContract = CategoryListService()) {
+        self.service = service
+    }
+    
+    func loadCategories() async -> Result<CategoryListResponse, Error> {
+        await service.loadCategories() // Normally we are doing any cache operation and also we convert from Network Model to Domain Model here
+    }
+}
